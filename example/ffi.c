97b7861f6a6019833a6a61f3641a8140fc664034
sugoiuguu <sugoiuguu@tfwno.gf>
2018-04-24 22:14:37 +0100

    added an experimental C/C++ interface

---
+++ example/ffi.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "glitch.h"
+
+/*
+ * $ cc -Wall -O2 -o ffi ffi.c glitch.a */
+
+int main(int argc, char *argv[])
+{
+    FILE *fp, *fpw;
+    char *buf;
+    Image_t *img;
+    int size;
+
+    if (argc < 2)
+        return 1;
+
+    fp = fopen(argv[1], "r");
+    
+    if (!fp)
+        return 1;
+
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+    rewind(fp);
+
+    buf = malloc(size);
+
+    if (!buf) {
+        fclose(fp);
+        return 1;
+    }
+
+    puts("glitching...");
+    fread(buf, size, 1, fp);
+    img = jumble_pixels("Y", buf, size);
+
+    if (!img) {
+        free(buf);
+        fclose(fp);
+        return 1;
+    }
+
+    fpw = fopen("out.png", "w");
+
+    if (!fpw) {
+        free(buf);
+        fclose(fp);
+        return 1;
+    }
+
+    fwrite(img->data, img->size, 1, fpw);
+
+    free(img->data);
+    free(img);
+    free(buf);
+    fclose(fp);
+    fclose(fpw);
+    puts("done...");
+
+    return 0;
+}
---
+++ example/ffi.go
@@ -0,0 +1,80 @@
+package main
+
+// $ go build -buildmode=c-archive -o glitch.a ffi.go
+
+/*
+#include <stdlib.h>
+
+typedef struct {
+    char *data;
+    size_t size;
+} Image_t;
+*/
+import "C"
+
+import (
+    "image"
+    "image/png"
+    _ "image/jpeg"
+    "bytes"
+    "unsafe"
+
+    "github.com/sugoiuguu/go-glitch"
+)
+
+// this does nothing...
+func main() {}
+
+//export jumble_pixels
+func jumble_pixels(expression, data *C.char, size C.int) *C.Image_t {
+    // compile expression
+    expr, err := glitch.CompileExpression(C.GoString(expression))
+    if err != nil {
+        println(1)
+        return nil
+    }
+
+    // write the raw image data to a bytes.Buffer
+    buf := new(bytes.Buffer)
+    if _,err := buf.Write(C.GoBytes(unsafe.Pointer(data), size)); err != nil {
+        println(2)
+        return nil
+    }
+
+    // decode image from bytes.Buffer
+    img, _, err := image.Decode(buf)
+    if err != nil {
+        println(err.Error())
+        return nil
+    }
+
+    // glitch that shit
+    glitchedimg, err := expr.JumblePixels(img)
+    if err != nil {
+        println(4)
+        return nil
+    }
+
+    // encode to a png
+    img = nil
+    buf.Reset()
+    if err := png.Encode(buf, glitchedimg); err != nil {
+        println(5)
+        return nil
+    }
+
+    // return raw image data
+    b := buf.Bytes()
+    l := C.size_t(len(b))
+    m := C.malloc(l)
+    if m == nil {
+        println(6)
+        return nil
+    }
+
+    s := (*C.Image_t)(m)
+    s.size = l
+    s.data = (*C.char)(C.CBytes(b))
+
+    return s
+}
